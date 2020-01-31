05363c2e6a7b9da5747ae7c9502c0296211b9710
Prashant Phulari <prashantphulari21@gmail.com>
2020-01-31 10:07:00 +0530

    New Changes

--- static/JS/main.js
+++ static/JS/main.js
@@ -1,14 +1,11 @@
 function getUserType(type){
     type = document.getElementById(type).value;
+    console.log("Hello");
     if(type=="alumni" || type=="student")
     {
+        document.getElementById('PRN_NO').style.display="table-row";
         document.getElementById('collegeselecttr').style.display="table-row";
         document.getElementById('dircollegeselect').style.display="none";
-    }
-    else if(type="sggs"){
-        document.getElementByTd('PRN_NO').style.display="table-row";
-        document.getElementById('collegeselecttr').style.display="table-row";
-
     }
     else if(type="DIR"){
         document.getElementById('dircollegeselect').style.display="table-row";
@@ -18,19 +15,5 @@ function getUserType(type){
         document.getElementById('userid').style.display="table-row";
         document.getElementById('password').style.display="table-row";
     }
-    
-}
 
-function getUserType1(type){
-    type = document.getElementById(type).value;
-    if(type=="alumni" || type=="student")
-    {
-        document.getElementById('collegeselecttr').style.display="table-row";
-        document.getElementById('dircollegeselect').style.display="none";
-    }
-     else if(type="sggs"){
-        document.getElementByTd('PRN_NO').style.display="table-row";
-        document.getElementById('collegeselecttr').style.display="table-row";
-
-    }
 }
